c29fca4284d29c665ff8d8181d050bee3ea36e98
antwolfe <muddywolfe77@gmail.com>
2021-10-25 11:34:41 -0500

    some exercise practice

---
+++ RB101/Exercises/Small-Problems/Easy/sum_of_digits.rb
@@ -0,0 +1,17 @@
+# Write a method that takes one argument, a positive integer, 
+# and returns the sum of its digits.
+
+def sum(int)
+  arr = []
+
+  arr << int
+  p arr
+  
+end
+
+
+
+
+# puts sum(23) # == 5
+# puts sum(496) == 19
+# puts sum(123_456_789) == 45
