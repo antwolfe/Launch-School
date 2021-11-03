2c81abf3830c91d2ee8478e245f42b21c6074389
antwolfe <muddywolfe77@gmail.com>
2021-11-02 22:50:17 -0500

    posted ver. of twenty-one

--- RB101/lesson_6/twenty_one.rb
+++ RB101/lesson_6/twenty_one.rb
@@ -1,4 +1,3 @@
-require 'pry'
 =begin
 
 1. Initialize deck
@@ -30,6 +29,37 @@ CARDS_HSH = {
   '10'    => 10
 }
 # rubocop:enable Layout/HashAlignment
+MAX_GAME_POINTS = 21
+DEALER_MIN_POINTS = 17
+ROUNDS = 5
+RULES = <<-HERE
+  The goal of Twenty-One is to try to get as close to 21 as possible, without going over. 
+  If you go over 21, it's a "bust" and you lose.
+
+  Setup: the game consists of a "dealer" and a "player". 
+  Both participants are initially dealt 2 cards. 
+  The player can see their 2 cards, but can only see one of the dealer's cards.
+
+  Card Values: the numbers 2 through 10 are worth their face value. 
+  The jack, queen, and king are each worth 10, and the ace can be worth 1 or 11. 
+  The ace's value is determined each time a new card is drawn from the deck. 
+  If the value is over 21 the ace's value becomes 1. 
+  
+  Player turn: the player goes first, and can decide to either "hit" or "stay". 
+  A hit means the player will ask for another card. 
+  Remember that if the total exceeds 21, then the player "busts" and loses. 
+  The turn is over when the player either busts or stays.
+  If the player busts, the game is over and the dealer won.
+
+  Dealer turn: when the player stays, it's the dealer's turn. 
+  The dealer must follow a strict rule for determining whether to hit or stay: 
+  hit until the total is at least 17. If the dealer busts, then the player wins.
+
+  Comparing cards: when both the player and the dealer stay, 
+  it's time to compare the total value of the cards 
+  Whoever has the higest value wins.
+
+  HERE
 
 def total(cards)
   sum = 0
@@ -52,12 +82,12 @@ def total(cards)
   sum
 end
 
-def user_turn(players_hand)
+def user_turn(user_hand, user_total, points)
   answer = nil
-
   loop do
-    display_hand_and_total("Player", players_hand)
-    break if busted?(players_hand)
+    user_total = total(user_hand)
+    display_hand_and_total("Your", user_hand, user_total)
+    break if busted?(user_total)
     puts '(h)it or (s)tay?'
     answer = gets.chomp.downcase
 
@@ -65,39 +95,44 @@ def user_turn(players_hand)
       break
     elsif %w(h hit).include?(answer)
       puts 'Hit!'
-      deal_card(players_hand)
+      deal_card(user_hand)
     else
       puts "Try Again"
     end
   end
 
-  if busted?(players_hand)
-    display_busted("Computer")
+  if busted?(user_total)
+    points[1] += 1
+    display_busted("Computer") # Computer wins
   else
     puts 'You chose to stay'
   end
 end
 
-def dealer_turn(dealers_hand)
+def dealer_turn(dealers_hand, dealers_total, points)
   loop do
-    display_hand_and_total("Dealer", dealers_hand)
-    break if total(dealers_hand) >= 17
+    dealers_total = total(dealers_hand)
+    display_hand_and_total("Dealer's", dealers_hand, dealers_total)
+    break if dealers_total >= DEALER_MIN_POINTS
     deal_card(dealers_hand)
   end
 
-  display_busted("Player") if busted?(dealers_hand)
+  if busted?(dealers_total)
+    points[0] += 1
+    display_busted("Player") # Player wins
+  end
 end
 
-def display_hand_and_total(player, current_hand)
+def display_hand_and_total(player, current_hand, player_total)
   puts "=============================================="
-  puts "#{player}'s hand is: #{show_hand(current_hand)}"
-  puts "#{player}'s total is: #{total(current_hand)}"
+  puts "#{player} hand is: #{show_hand(current_hand)}"
+  puts "#{player} total is: #{player_total}"
   puts "=============================================="
   newline
 end
 
-def busted?(cards)
-  total(cards) > 21
+def busted?(player_total)
+  player_total > MAX_GAME_POINTS
 end
 
 def display_busted(other_player)
@@ -114,16 +149,15 @@ def show_hand(cards)
   cards.to_s
 end
 
-def calculate_winner(players_hand, dealers_hand)
-  players_total = total(players_hand)
-  dealers_total = total(dealers_hand)
-
-  if dealers_total == players_total
+def calculate_winner(user_total, dealers_total, points)
+  if user_total == dealers_total
     puts "It's a tie"
-  elsif players_total > dealers_total
+  elsif user_total > dealers_total
     puts "Player won"
+    points[0] += 1
   else
     puts "Computer won"
+    points[1] += 1
   end
 end
 
@@ -139,34 +173,47 @@ def newline
 end
 
 # Game Loop
+points = [0, 0]
+
 loop do
   system 'clear'
   puts "Welcome to Twenty-One"
   newline
+  puts RULES
+  newline
+  puts "Rounds will automatically play to 5, " \
+       "you can exit between them by pushing 'n'."
+  newline
 
-  players_hand = []
+  user_hand = []
   dealers_hand = []
 
   2.times do
-    deal_card(players_hand)
+    deal_card(user_hand)
     deal_card(dealers_hand)
   end
 
-  # puts "Your hand is: #{show_hand(players_hand)}"
-  # puts "Your total is: #{total(players_hand)}"
+  user_total = total(user_hand)
+  dealers_total = total(dealers_hand)
+
   puts "Dealer's hand is: #{show_hand(dealers_hand[0])} and [??]"
   newline
 
   loop do
-    break if user_turn(players_hand) == "Busted"
-    newline
-    break if dealer_turn(dealers_hand) == "Busted"
-    newline
-    calculate_winner(players_hand, dealers_hand)
+    break if user_turn(user_hand, user_total, points) == "Busted"
+    break if dealer_turn(dealers_hand, dealers_total, points) == "Busted"
+    user_total = total(user_hand)
+    dealers_total = total(dealers_hand)
+    calculate_winner(user_total, dealers_total, points)
     break
   end
 
-  break unless play_again?
+  if points[0] == ROUNDS || points[1] == ROUNDS
+    puts "5 rounds completed -- Player: #{points[0]} / Dealer: #{points[1]}"
+    break
+  elsif !play_again?
+    break
+  end
 end
 
 puts "Thanks for playing. Goodbye!"
